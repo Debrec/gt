96f7c2a312cf50230a07fa97f17f14f4c61f89db
Hernán Braile <hbraile@outlook.com.ar>
2017-08-22 13:14:17 -0300

    update

--- conectar.php
+++ conectar.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("127.0.0.1", "hernan", "test", "gt");
+$mysqli = new mysqli("127.0.0.1", "hernan", "test", "gtdevel");
 if ($mysqli === false) {
   die("ERROR: Could not connect. " . mysqli_connect_error());
 }
