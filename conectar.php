9aa16b4d9c23177f757fb5e015b0df3b785ca18e
Hernán Braile <hbraile@outlook.com.ar>
2017-08-22 13:20:49 -0300

    correccion de errores

--- conectar.php
+++ conectar.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("127.0.0.1", "hernan", "test", "gtdevel");
+$mysqli = new mysqli("127.0.0.1", "hernan", "test", "gt");
 if ($mysqli === false) {
   die("ERROR: Could not connect. " . mysqli_connect_error());
 }
--- superior.php
+++ superior.php
@@ -1,3 +1,3 @@
 <table width="100%">
-<tr><td><img width="67px" height="54px" src="./img/logo.png"></td><td><span><h1>Version de desarrollo</h1><!--<h1>Gestor de tareas</h1>--></span></td></tr>
+<tr><td><img width="67px" height="54px" src="./img/logo.png"></td><td><span></td><td><span><img width="304px" height="30px" src="./img/titulo2.png"><!--<h1>Gestor de tareas</h1>--></span></td></tr>
 </table>
