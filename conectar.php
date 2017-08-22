e476873389924b0bda421a8f294212de64a1121f
Hernán Braile <hbraile@outlook.com.ar>
2017-08-22 13:25:56 -0300

    fixed untracked files

--- conectar.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = new mysqli("127.0.0.1", "hernan", "test", "gt");
-if ($mysqli === false) {
-  die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-$mysqli->set_charset('utf8');
-?>
--- superior.php
+++
@@ -1,3 +0,0 @@
-<table width="100%">
-<tr><td><img width="67px" height="54px" src="./img/logo.png"></td><td><span></td><td><span><img width="304px" height="30px" src="./img/titulo2.png"><!--<h1>Gestor de tareas</h1>--></span></td></tr>
-</table>
